c8d56d0bca7b849d9e430dcb1dedc7339d3ccb7d
CristhianRichard94 <richardcristhian94@gmail.com>
2016-09-16 20:24:10 -0300

    Ej2 TP3

---
+++ TP3/Ej2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal[] animales = new Animal[6];
+            animales[0] = new Perro();
+            animales[1] = new Gato();
+            animales[2] = new Pato();
+            animales[3] = new Pollito();
+            animales[4] = new Cerdo();
+            animales[5] = new Gallo();
+            Veterinaria vet = new Veterinaria();
+            vet.AceptarAnimal(animales);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TP3/Ej2/Veterinaria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej2
+{
+    class Veterinaria
+    {
+        public void AceptarAnimal(Animal[] animales)
+        {
+
+            foreach (Animal animal in animales)
+            {
+                animal.HacerRuido();
+            }
+        }
+    }
+}
